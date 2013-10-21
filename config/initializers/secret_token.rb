d8575c20a89604f3155eb528043641897df75246
analyticsPierce <jason@marketingscience.co>
2013-10-21 12:27:32 -0600

    load initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MsdSite::Application.config.secret_key_base = 'c9333b1bb0e0af62732220d4cf83854281fc3c28f1b719c913a8f812e2823d977e068e413e7c672c6027e76db4f140eac30c558681471de0289b94f48b24e220'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MsdSite::Application.config.session_store :cookie_store, key: '_msd-site_session'
